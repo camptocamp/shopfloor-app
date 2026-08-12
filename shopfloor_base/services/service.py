45641264a42e04e2ac0f79402c1b6ffa96500e09
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2026-08-12 12:03:22 +0200

    fixup! fixup! shopfloor_base: add common function for jump_to_menu

--- shopfloor_base/services/service.py
+++ shopfloor_base/services/service.py
@@ -130,17 +130,16 @@ class BaseShopfloorService(AbstractComponent):
         return response
 
     def _response_for_jump_to_menu(self, menu, message=None):
-        # Get the data for the start state
         service = self.component(menu.scenario_id.key)
         service.work.menu = menu
-        jump_to_data = service._response_for_start()
+        jump_to_data = service._get_data_for_jump_to_menu()
         jump_to_state = jump_to_data["next_state"]
+        states_data = {jump_to_state: jump_to_data["data"][jump_to_state]}
         data = {
             "next_state": jump_to_state,
             "menu_id": menu.id,
-            "state_data": jump_to_data["data"][jump_to_state],
+            "states_data": states_data,
         }
-
         return self._response(
             next_state="jump_to_menu",
             data=data,
